76c1e5dd201b68ffd5957edcc6118423a179369b
Ahmad Aiman <aimanzulbahari@gmail.com>
2023-06-09 07:51:57 +0800

    fix the javascript file
    1. Typo in variable name: In the createPassword function, you have a typo in the variable name lenght (missing 't'). It should be length to match the variable declaration at the beginning.
    
    2. Infinite loop: The while loop in the createPassword function is causing an infinite loop because you are assigning a single character from allChars to the password variable on each iteration. Instead, you should be appending characters to the existing password variable using the += operator.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const passwordBox = document.getElementById("password");
-const lenght = 12;
+const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -9,19 +9,20 @@ const symbol = "!@#$%&'()*+,-./";
 const allChars = upperCase + lowerCase + symbol + number;
 
 function createPassword() {
-    let password = "";
-    password += upperCase[Math.floor(Math.random() *upperCase.length)];
-    password += upperCase[Math.floor(Math.random() *lowerCase.length)];
-    password += upperCase[Math.floor(Math.random() *number.length)];
-    password += upperCase[Math.floor(Math.random() *symbol.length)];
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
 
-    while (lenght > password.length) {
-        password = allChars[Math.floor(Math.random() * allChars.length)];
-    }
-    passwordBox.value = password;
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  
+  passwordBox.value = password;
 }
 
 function copyPassword() {
-    passwordBox = select();
-    document.execCommand('copy');
+  passwordBox.select();
+  document.execCommand('copy');
 }
